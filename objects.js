3fc106d443c6c3878281a6cebc93f104a4ec01ff
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-19 21:31:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(playlist, cafres, momento) {
   playlist[cafres] = "momento";
   return playlist;
 }
+
+function removeFromPlaylist(playlist, cafres) {
+  delete playlist.cafres;
+  return playlist;
+}
